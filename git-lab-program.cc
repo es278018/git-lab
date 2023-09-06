b4a59d2c20d6074b110fd0e53ea505e0a75d80a4
es278018 <es278018@ohio.edu>
2023-09-06 18:49:29 -0400

    Initial commit for this one

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *      File: git-lab-program.cc
+ *      Author: Adelle Stratton
+ *      Date: 09/06/23
+ *      Description: Program meant for lab 2 in CS2400
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+ int main(int argc, char *argv[])
+ {
+     cout << "Hello Git!!" << endl;
+     return 0;
+ } // main
